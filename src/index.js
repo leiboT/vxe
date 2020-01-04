71ef1ba664df2b3f86b56f7ddd52bfb8178fba25
leib <leib@soonfor.com>
2020-01-04 11:36:20 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+var vm = new Vue({
+    el: "#root",
+    data: function () {
+        return {
+            message: "hello world",
+            visible: false,
+            tableData: [{
+                name: "aa"
+            }],
+            allAlign: "center"
+        }
+    },
+    methods: {
+
+    }
+})
